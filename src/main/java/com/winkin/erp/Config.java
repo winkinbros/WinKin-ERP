a4ce7a90f38e9c860f0ffe6ce160cd348b2e46ff
VARGHESH ALWIN RAJA C <dev.winkinbros@gmail.com>
2019-03-09 19:40:33 +0530

    Database Config Done

--- src/main/java/com/winkin/erp/Config.java
+++ src/main/java/com/winkin/erp/Config.java
@@ -1,5 +1,68 @@
 package com.winkin.erp;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:db.properties")
+@PropertySource("classpath:mail.properties")
+@EnableTransactionManagement
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
+@ComponentScan(basePackages = { "com.winkin.erp.*" })
 public class Config {
 
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource getDataBaseSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setDataSource(getDataBaseSource());
+
+        Properties props = new Properties();
+        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        props.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+
+        factoryBean.setHibernateProperties(props);
+        factoryBean.setAnnotatedClasses();
+        return factoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
+    }
 }
