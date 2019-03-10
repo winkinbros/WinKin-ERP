640da66ad9f13f907082378a2c9f3f55de50a88b
VARGHESH ALWIN RAJA C <dev.winkinbros@gmail.com>
2019-03-10 09:44:43 +0530

    Auth conroller added

---
+++ src/main/java/com/winkin/erp/controller/core/AuthController.java
@@ -0,0 +1,11 @@
+package com.winkin.erp.controller.core;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("auth")
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    
+}
