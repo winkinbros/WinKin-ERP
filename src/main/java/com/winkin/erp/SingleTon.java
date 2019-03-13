f17088b7424d579fa3ea0c74845889d4a4d2d106
CVAR <alwinraja.kryon@gmail.com>
2019-03-13 19:52:43 +0530

    Dashboard and default redirect added.

---
+++ src/main/java/com/winkin/erp/SingleTon.java
@@ -0,0 +1,48 @@
+package com.winkin.erp;
+
+import javax.servlet.http.HttpSession;
+
+
+public class SingleTon {
+
+	private static final String SESSION_DEFAULT_ROLE_ID = "roleid";
+	private static final String SESSION_USER_ID = "userid";
+	private static SingleTon singleTon;
+
+	private SingleTon() {
+
+	}
+
+	public static final SingleTon getInstance() {
+
+		if (singleTon == null) {
+			singleTon = new SingleTon();
+		}
+
+		return singleTon;
+	}
+
+	
+	
+
+	
+	public static void removeSession(HttpSession httpSession) {
+
+		httpSession.removeAttribute("status");
+
+	}
+
+	public static String getSessionUserId(HttpSession httpSession) {
+
+		return (String) httpSession.getAttribute(SingleTon.SESSION_USER_ID);
+
+	}
+
+	public static Integer getSessionRoleId(HttpSession httpSession) {
+
+		return (Integer) httpSession.getAttribute(SingleTon.SESSION_DEFAULT_ROLE_ID);
+
+	}
+
+	
+}
