f30d3a716e598933067ee33b28d19120f181b8cd
CVAR <alwinraja.kryon@gmail.com>
2019-03-13 19:52:14 +0530

    Default  Dashboard Redirection Done and Singlton Added

--- src/main/java/com/winkin/erp/controller/core/AuthController.java
+++ src/main/java/com/winkin/erp/controller/core/AuthController.java
@@ -1,18 +1,13 @@
 package com.winkin.erp.controller.core;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 
 @Controller("auth")
 @RequestMapping("/auth")
@@ -20,11 +15,10 @@ public class AuthController {
 
 
 
-    @GetMapping(value = {"/page/signin"})
+    @GetMapping(value = {"/signin"})
     public ModelAndView signinPage(HttpSession httpSession, Model model) {
-
-
-        model.addAttribute("test","test");
+    	
+    	
 
         return new ModelAndView("login");
     }
