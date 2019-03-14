e054fccda2e5f0944548903a94f3ad2ac3609331
VARGHESH ALWIN RAJA C <dev.winkinbros@gmail.com>
2019-03-14 18:22:51 +0530

    Dashboard Layout Added

--- src/main/java/com/winkin/erp/controller/core/DefaultController.java
+++ src/main/java/com/winkin/erp/controller/core/DefaultController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.winkin.erp.SingleTon;
+
 @Controller("/")
 @RequestMapping()
 public class DefaultController {
@@ -16,14 +18,14 @@ public class DefaultController {
     @GetMapping(value = {""})
     public ModelAndView signinPage(HttpSession httpSession, Model model) {
 
-    	if(httpSession.getAttribute("id") == null ) {
-            return new ModelAndView("redirect:auth/signin");
-
-    	}
+//    	if(httpSession.getAttribute("id") == null ) {
+//            return new ModelAndView("redirect:auth/signin");
+//
+//    	}
     	
     	
 
-        return new ModelAndView("dashboard");
+        return new ModelAndView(SingleTon.PAGE_DASHBOARD);
     }
 
 	
