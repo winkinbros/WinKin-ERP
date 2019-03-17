cbe63e971d1d6640c8e18a790929719cb10630f3
VARGHESH ALWIN RAJA C <dev.winkinbros@gmail.com>
2019-03-17 17:41:58 +0530

    Notes Ref work

--- src/main/java/com/winkin/erp/controller/notes/NotesController.java
+++ src/main/java/com/winkin/erp/controller/notes/NotesController.java
@@ -5,33 +5,41 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.winkin.erp.SingleTon;
 import com.winkin.erp.pojo.PJ_NOTES;
 import com.winkin.erp.service.UserService;
 
 @Controller("notes")
 @RequestMapping("/app/notes")
 public class NotesController {
-	
-	
+
 	@Autowired
 	private UserService I_UserService;
-	
-	
-	@PostMapping(value = { "/add" })
-	public ModelAndView post_add(HttpSession httpSession, Model model, @RequestBody PJ_NOTES RO_PJ_NOTES) {
+
+	@GetMapping(value = { "","/" })
+	public ModelAndView post_add(HttpSession httpSession, Model model) {
+
+		if (SingleTon.isNewSession(httpSession)) {
+
+			return new ModelAndView("redirect:auth/signin");
+
+		}
+
+		model.addAttribute("test","STRING TEXT");
 		
-			
 		
-		
-		
-		return null;
-		
+		return new ModelAndView(SingleTon.PAGE_NOTES_HOME);
+
 	}
+	
+	
+	
 
 }
